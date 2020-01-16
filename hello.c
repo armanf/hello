4e73effba51ebddfc8133fc03f000f6cc6980730
armanf <59975385+armanf@users.noreply.github.com>
2020-01-16 12:47:44 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+ /* "Hello World", My first program
+Name: Arman Radmanesh
+Date: 1/14/2020
+*/
+#include <stdio.h>
+ 
+ int main {int argc char **argv){
+	printf("Arman Radmanesh\n");
++ 	printf("Cyber Security\n");
+	return 0;
+
+}
